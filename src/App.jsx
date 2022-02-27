8284e0d7da15e1c515c9fbf3fae2b05d3e5b7009
sikander-afzaal <sikander.aaa@gmail.com>
2022-02-27 20:00:58 +0500

    first

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,25 @@ import head from "./Assets/1.png";
 import img1 from "./Assets/2.png";
 import img2 from "./Assets/3.png";
 import img3 from "./Assets/4.png";
+import img5 from "./Assets/5.png";
+import img6 from "./Assets/6.png";
 import side from "./Assets/Layer 1.png";
+import side2 from "./Assets/Layer 2.png";
 function App() {
   return (
     <div className="App">
       <img src={side} alt="" className="absolute" />
+      <img src={side2} alt="" className="absolute2" />
       <div className="container">
         <img src={head} alt="" className="header" />
         <div className="section1">
           <img className="title" src={img1} alt="" />
           <img className="desc" src={img2} alt="" />
           <img className="desc" src={img3} alt="" />
+        </div>
+        <div className="section1">
+          <img className="title" src={img5} alt="" />
+          <img className="desc" src={img6} alt="" />
         </div>
       </div>
     </div>
