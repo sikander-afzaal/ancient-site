84661491d0ae6265ee6db8d1d65553b98675be63
sikander-afzaal <sikander.aaa@gmail.com>
2022-03-01 11:53:51 +0500

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+import { React, useState } from "react";
 import "./App.css";
 import head from "./Assets/1.png";
 import img1 from "./Assets/2.png";
@@ -36,135 +36,149 @@ import team2 from "./Assets/team/team (2).png";
 import team3 from "./Assets/team/team (3).png";
 import team4 from "./Assets/team/team (4).png";
 import team5 from "./Assets/team/team (5).png";
+import Mint from "./Pages/Mint/Mint.jsx";
 function App() {
+  const [mint, setMint] = useState(false);
   return (
-    <>
-      <div className="App">
-        <div className="navbar">
-          <div className="left-navbar">
-            <a href="#" className="nav-link">
-              <img src={opensea_icon} alt="" />
-            </a>
-            <a href="#" className="nav-link">
-              <FaTwitter />
-            </a>
-            <a href="discord.gg/N5wDdjtDVZ" className="nav-link">
-              <FaDiscord />
-            </a>
-          </div>
-          <div className="right-navbar">
-            <button className="mint">Mint</button>
-          </div>
+    <div className="main-wrapper">
+      <div className="navbar">
+        <div className="left-navbar">
+          <a href="#" className="nav-link">
+            <img src={opensea_icon} alt="" />
+          </a>
+          <a href="#" className="nav-link">
+            <FaTwitter />
+          </a>
+          <a
+            href="https://discord.com/invite/N5wDdjtDVZ"
+            target={"blank"}
+            className="nav-link"
+          >
+            <FaDiscord />
+          </a>
         </div>
-        <img src={side} alt="" className="absolute" />
-        <img src={side2} alt="" className="absolute2" />
-        <img src={side3} alt="" className="absolute3" />
-        <img src={side4} alt="" className="absolute4" />
-        <img src={side5} alt="" className="absolute5" />
-        <div className="container">
-          <img src={head} alt="" className="header" />
-          <div className="section1">
-            <img className="title" src={img1} alt="" />
-            <img className="desc" src={img2} alt="" />
-            <img className="desc" src={img3} alt="" />
-          </div>
-          <div className="section1">
-            <img className="title max28" src={img5} alt="" />
-            <img className="desc" src={img6} alt="" />
-          </div>
-          <div className="section1">
-            <img className="title" src={img7} alt="" />
-            <img className="desc" src={img8} alt="" />
-          </div>
-          <div className="section1">
-            <img className="title" src={img9} alt="" />
-            <img className="desc" src={img22} alt="" />
-            <img className="desc max-80" src={img10} alt="" />
-          </div>
-          <div className="section1">
-            <img className="title" src={img11} alt="" />
-            <img className="desc max-50" src={img12} alt="" />
-            <img className="desc adjust" src={img13} alt="" />
-            <img className="desc max-50" src={img14} alt="" />
-            <img className="desc max-52" src={img15} alt="" />
-          </div>
-          <div className="section1">
-            <img className="title" src={img16} alt="" />
-            <img className="desc" src={img17} alt="" />
-            <img className="desc" src={img18} alt="" />
-            <img className="desc" src={img19} alt="" />
-            <img className="desc" src={img20} alt="" />
-          </div>
-          <div className="section1">
-            <img className="desc" src={img21} alt="" />
-          </div>
-          <img src={side6} alt="" className="absolute6" />
+        <div className="right-navbar">
+          <button onClick={() => setMint((prev) => !prev)} className="mint">
+            mint
+          </button>
         </div>
       </div>
-      <div className="team">
-        <h1>Founding Team</h1>
-        <div className="wrapper">
-          <div className="team-box">
-            <a
-              target={"blank"}
-              href="https://twitter.com/thepoette"
-              className="team-img"
-            >
-              <img src={team2} alt="" />
-            </a>
-            <div className="name">Poette</div>
-            <div className="position">
-              Poetess & <br />
-              Creative Director
+      {mint ? (
+        <Mint />
+      ) : (
+        <>
+          <div className="App">
+            <img src={side} alt="" className="absolute" />
+            <img src={side2} alt="" className="absolute2" />
+            <img src={side3} alt="" className="absolute3" />
+            <img src={side4} alt="" className="absolute4" />
+            <img src={side5} alt="" className="absolute5" />
+            <div className="container">
+              <img src={head} alt="" className="header" />
+              <div className="section1">
+                <img className="title" src={img1} alt="" />
+                <img className="desc" src={img2} alt="" />
+                <img className="desc" src={img3} alt="" />
+              </div>
+              <div className="section1">
+                <img className="title max28" src={img5} alt="" />
+                <img className="desc" src={img6} alt="" />
+              </div>
+              <div className="section1">
+                <img className="title" src={img7} alt="" />
+                <img className="desc" src={img8} alt="" />
+              </div>
+              <div className="section1">
+                <img className="title" src={img9} alt="" />
+                <img className="desc" src={img22} alt="" />
+                <img className="desc max-80" src={img10} alt="" />
+              </div>
+              <div className="section1">
+                <img className="title" src={img11} alt="" />
+                <img className="desc max-50" src={img12} alt="" />
+                <img className="desc adjust" src={img13} alt="" />
+                <img className="desc max-50" src={img14} alt="" />
+                <img className="desc max-52" src={img15} alt="" />
+              </div>
+              <div className="section1">
+                <img className="title" src={img16} alt="" />
+                <img className="desc" src={img17} alt="" />
+                <img className="desc" src={img18} alt="" />
+                <img className="desc" src={img19} alt="" />
+                <img className="desc" src={img20} alt="" />
+              </div>
+              <div className="section1">
+                <img className="desc" src={img21} alt="" />
+              </div>
+              <img src={side6} alt="" className="absolute6" />
             </div>
           </div>
-          <div className="team-box">
-            <a
-              href="https://twitter.com/D34thSt4lker"
-              target={"blank"}
-              className="team-img"
-            >
-              <img src={team5} alt="" />
-            </a>
-            <div className="name">D34thst4lker</div>
-            <div className="position">Developer</div>
+          <div className="team">
+            <h1>Founding Team</h1>
+            <div className="wrapper">
+              <div className="team-box">
+                <a
+                  target={"blank"}
+                  href="https://twitter.com/thepoette"
+                  className="team-img"
+                >
+                  <img src={team2} alt="" />
+                </a>
+                <div className="name">Poette</div>
+                <div className="position">
+                  Poetess & <br />
+                  Creative Director
+                </div>
+              </div>
+              <div className="team-box">
+                <a
+                  href="https://twitter.com/D34thSt4lker"
+                  target={"blank"}
+                  className="team-img"
+                >
+                  <img src={team5} alt="" />
+                </a>
+                <div className="name">D34thst4lker</div>
+                <div className="position">Developer</div>
+              </div>
+              <div className="team-box">
+                <a
+                  href="https://www.instagram.com/ericallenphoto/"
+                  target={"blank"}
+                  className="team-img"
+                >
+                  <img src={team4} alt="" />
+                </a>
+                <div className="name">Eric</div>
+                <div className="position">Photography</div>
+              </div>
+              <div className="team-box">
+                <a
+                  href="https://twitter.com/JJNegev"
+                  target={"blank"}
+                  className="team-img"
+                >
+                  <img src={team1} alt="" />
+                </a>
+                <div className="name">JJ</div>
+                <div className="position">Project Manager</div>
+              </div>
+              <div className="team-box">
+                <a
+                  href="https://twitter.com/jagmot80"
+                  target={"blank"}
+                  className="team-img"
+                >
+                  <img src={team3} alt="" />
+                </a>
+                <div className="name">Jagmot</div>
+                <div className="position">Advisor</div>
+              </div>
+            </div>
           </div>
-          <div className="team-box">
-            <a
-              href="https://www.instagram.com/ericallenphoto/"
-              target={"blank"}
-              className="team-img"
-            >
-              <img src={team4} alt="" />
-            </a>
-            <div className="name">Eric</div>
-            <div className="position">Photography</div>
-          </div>
-          <div className="team-box">
-            <a
-              href="https://twitter.com/JJNegev"
-              target={"blank"}
-              className="team-img"
-            >
-              <img src={team1} alt="" />
-            </a>
-            <div className="name">JJ</div>
-            <div className="position">Project Manager</div>
-          </div>
-          <div className="team-box">
-            <a
-              href="https://twitter.com/jagmot80"
-              target={"blank"}
-              className="team-img"
-            >
-              <img src={team3} alt="" />
-            </a>
-            <div className="name">Jagmot</div>
-            <div className="position">Advisor</div>
-          </div>
-        </div>
-      </div>
-    </>
+        </>
+      )}
+    </div>
   );
 }
 
---
+++ src/Pages/Mint/Mint.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import "./Mint.css";
+function Mint() {
+  const [total, setTotal] = useState(1);
+  return (
+    <div className="mint-div">
+      <div className="mint-box">
+        <h1>Cosmic Poems</h1>
+        <div className="calc-div">
+          <div className="left-calc">
+            <button
+              onClick={() => setTotal((prev) => prev + 1)}
+              className="func"
+            >
+              +
+            </button>
+            <p className="num">{total}</p>
+            <button
+              onClick={() => {
+                if (total > 0) {
+                  setTotal((prev) => prev - 1);
+                }
+              }}
+              className="func"
+            >
+              -
+            </button>
+          </div>
+          <p>Mint</p>
+        </div>
+        <p className="total">0/333 minted</p>
+        <p>Pre-sale begins on March 20th, 2022</p>
+        <p>Public sale is March 21st, 2022</p>
+      </div>
+    </div>
+  );
+}
+
+export default Mint;
